09f528227ec9c8199b09ba7d102cdbe523ff999b
Georgiy <dev.georgiy@gmail.com>
2021-09-22 18:23:32 +0300

    Норм визуал, отрисовка формы по клику, перезгрузка страницы при переходе назад

--- public/main.js
+++ public/main.js
@@ -16,14 +16,59 @@ const configApp = {
     profile: {
         href: '/profile',
         name: 'Profile'
+    },
+    abount: {
+        href: '/about',
+        name: 'About'
     }
 }
 
 /* В map передается массив из ключа и объека-значения. 
 Мы его деструктим [], затем также деструктим объект {} */
-Object.entries(configApp).map(([key, {href, name}]) => {
+Object.entries(configApp).forEach(([key, {href, name}]) => {
     const menuElement = document.createElement('a'); // Созадем тег 
     menuElement.href = href;
     menuElement.textContent = name;
+    menuElement.id = key;
     root.appendChild(menuElement);
+});
+
+
+const createFormInput = (type, text, name) => {
+    const input = document.createElement('input');
+    input.type = type
+    input.name = name;
+    input.placeholder = name;
+    input.text = text;
+    return input;
+}
+
+const signup = document.getElementById('signup');
+console.log(signup);
+signup.addEventListener('click', (event)  => {
+    event.preventDefault(); // Отключаем default поведение - переход по ссылке
+    root.innerHTML = '';
+    
+    const form = document.createElement('form');
+    
+    form.appendChild(createFormInput('text', 'Логин:', 'login'))
+    form.appendChild(createFormInput('password', 'Логин:', 'password1'))
+    form.appendChild(createFormInput('password', 'Пароль:', 'password2'))
+
+    const btn = createFormInput('submit', 'Зарегистрироваться!', 'submit')
+    form.appendChild(btn);
+    btn.addEventListener('click', (event) => {
+        console.log('submint click!');
+    });
+    
+    const back = document.createElement('a');
+    form.appendChild(back);
+    back.textContent = 'Назад';
+    back.href = '/menu';
+    back.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.location.href = '/';  // Приводит к загрузке станицы по '/'. Требует время.
+    });
+    
+    root.appendChild(form);
 });
