6f8a1ef266b0092377919be1c7fc346482edeeef
Georgiy <dev.georgiy@gmail.com>
2021-09-22 18:43:33 +0300

    Переключение визуала через JS

--- public/main.js
+++ public/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('code');
-
 const root = document.getElementById('root');
 
 const configApp = {
@@ -23,16 +21,25 @@ const configApp = {
     }
 }
 
-/* В map передается массив из ключа и объека-значения. 
-Мы его деструктим [], затем также деструктим объект {} */
-Object.entries(configApp).forEach(([key, {href, name}]) => {
-    const menuElement = document.createElement('a'); // Созадем тег 
-    menuElement.href = href;
-    menuElement.textContent = name;
-    menuElement.id = key;
-    root.appendChild(menuElement);
-});
+function showMenu() {
+    root.innerHTML = '';
+    /* В map передается массив из ключа и объека-значения. 
+    Мы его деструктим [], затем также деструктим объект {} */
+    Object.entries(configApp).forEach(([key, { href, name }]) => {
+        const menuElement = document.createElement('a'); // Созадем тег 
+        menuElement.href = href;
+        menuElement.textContent = name;
+        menuElement.id = key;
+        root.appendChild(menuElement);
+    });
 
+    const signup = document.getElementById('signup');
+    signup.addEventListener('click', (event) => {
+        event.preventDefault(); // Отключаем default поведение - переход по ссылке
+        showSignUp();
+    });
+
+}
 
 const createFormInput = (type, text, name) => {
     const input = document.createElement('input');
@@ -43,14 +50,11 @@ const createFormInput = (type, text, name) => {
     return input;
 }
 
-const signup = document.getElementById('signup');
-console.log(signup);
-signup.addEventListener('click', (event)  => {
-    event.preventDefault(); // Отключаем default поведение - переход по ссылке
+function showSignUp() {
     root.innerHTML = '';
-    
+
     const form = document.createElement('form');
-    
+
     form.appendChild(createFormInput('text', 'Логин:', 'login'))
     form.appendChild(createFormInput('password', 'Логин:', 'password1'))
     form.appendChild(createFormInput('password', 'Пароль:', 'password2'))
@@ -60,15 +64,17 @@ signup.addEventListener('click', (event)  => {
     btn.addEventListener('click', (event) => {
         console.log('submint click!');
     });
-    
+
     const back = document.createElement('a');
     form.appendChild(back);
     back.textContent = 'Назад';
     back.href = '/menu';
     back.addEventListener('click', (event) => {
         event.preventDefault();
-        document.location.href = '/';  // Приводит к загрузке станицы по '/'. Требует время.
+        showMenu();
     });
-    
+
     root.appendChild(form);
-});
+}
+
+showMenu();
