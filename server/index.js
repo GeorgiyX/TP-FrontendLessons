f3508a1a25fa36044a1946c15643921f03c950ca
Georgiy <dev.georgiy@gmail.com>
2021-09-22 11:29:09 +0300

    Добавили асинхронное чтени файла страницы (для обработки ошибки)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+'use strict'; // Активация фич ES5+, не совместимых со старым JS. Webpack () это добавляет автоматом.
+
+// Require - функция nodejs
 const http = require('http');  // Пакет для работы с HTTP
 const fs = require('fs');
+const { strict } = require('assert/strict');
+
+const page404 = fs.readFileSync('./public/404.html'); // Подгтовим заранее страницу 404, чтобы не чистать ее раждый раз
 
 const server = http.createServer((req, res) => {
-    const path = req.url; // GET /index.html HTTP/1.1
-    const file = fs.readFileSync(`./public${path}`) // Синхронное чтение файла
-    res.write(file);
-    res.end(); // Ответ нужно обязательно завершить чтобы клиент не ждал бесконечно.
+    const path = req.url === '/' ? '/index.html' : req.url; // GET /index.html HTTP/1.1
+    fs.readFile(`./public${path}`, (error, data) => {
+        if (error) { 
+            res.statusCode = 404;
+            data = page404; 
+        }
+        res.write(data);
+        res.end(); // Ответ нужно обязательно завершить чтобы клиент не ждал бесконечно.
+    }); // Асинхронное чтение файла, обработка ошибки. Профит именно в отсутствии try-catch
+    
 });
 
 console.log('Listening at http://localhost:4411')
